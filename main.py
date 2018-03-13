24301b27b57e6a8801b98fba219ba9d70758b355
soovoofe <pe4enkus@gmail.com>
2018-03-13 19:19:57 +0200

    Simple functionality

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ TOKEN = "574980005:AAEpFevnWRl6m3GYzFYFMhULbX2FCp8xmZg"
 
 bot = telebot.TeleBot(TOKEN)
 
+@bot.message_handler(commands=['start', 'help'])
+def proc_simple_message(message):
+    bot.reply_to(message, 'bot testing message')
+
+bot.polling()
