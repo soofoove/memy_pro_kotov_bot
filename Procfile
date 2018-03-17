c40d4dae104c31ffbcef669c866ee6e824d1fbf3
soovoofe <pe4enkus@gmail.com>
2018-03-17 02:55:12 +0200

    some structurizing

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-worker: python main.py
+worker: python executable/main.py
--- main.py
+++
@@ -1,9 +0,0 @@
-from mem_bot import *
-
-if __name__ == '__main__':
-    try:
-        MemBot = MemyProKotovBot()
-        MemBot.updates_listener_start()
-    except vk_api.AuthError as error_msg:
-        print(error_msg) 
-        
